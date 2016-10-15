fba9a402a72a841feb4dc15d977c5b12178ca4e9
Benjamin Coe <ben@npmjs.com>
2016-10-15 15:22:53 -0700

    feat: add pirate name generator

---
+++ commands/name.js
@@ -0,0 +1,30 @@
+const suffixes = [
+  'the Red',
+  'the Sour',
+  'the Strong',
+  'the Grumpy',
+  'the Stout'
+  '',
+  '',
+  '',
+  ''
+]
+
+const prefixes = [
+  "Lil'",
+  'Peg-legged',
+  'Two-Legged'
+  'One-Eyed',
+  'Two-Eyed',
+  'Four-Eyed',
+  'Smelly'
+]
+
+exports.command = 'name'
+exports.describe = "what's yer pirate name?"
+exports.builder = {}
+exports.handler = function (argv) {
+  const prefix = prefixes[parseInt(Math.random() * prefixes.length)]
+  const suffix = suffixes[parseInt(Math.random() * prefixes.length)]
+  argv.respond(`${prefix} ${argv.user_name} ${suffix}`)
+}
